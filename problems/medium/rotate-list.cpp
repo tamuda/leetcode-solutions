a76683dd3a348a6c40fba0ae5e6816b51684089b
Tamuda <118585051+tamuda@users.noreply.github.com>
2025-07-13 08:04:08 -0400

    Solve Rotate List [Medium] - cpp

---
+++ problems/medium/rotate-list.cpp
@@ -0,0 +1,33 @@
+/**
+ * LeetCode Problem: Rotate List
+ * Difficulty: Medium
+ * Language: cpp
+ * Problem URL: https://leetcode.com/problems/rotate-list/description/
+ * 
+ * Auto-committed by LeetUp
+ * Date: 2025-07-13T12:04:07.921Z
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        
+    }
+};
+/*
+ * End of solution for: Rotate List
+ * Committed at: 2025-07-13T12:04:07.921Z
+ * 
+ * This solution was automatically committed by LeetUp.
+ * Visit: https://leetup.app
+ */
