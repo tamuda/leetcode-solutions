48558fa6d18737a58e9b3e5c320c30832ba51483
Tamuda <118585051+tamuda@users.noreply.github.com>
2025-07-31 18:51:16 -0400

    Solve Two Sum [Easy] - python

--- problems/easy/two-sum.py
+++ problems/easy/two-sum.py
@@ -2,10 +2,10 @@
  * LeetCode Problem: Two Sum
  * Difficulty: Easy
  * Language: python
- * Problem URL: https://leetcode.com/problems/two-sum/submissions/1718296330/
+ * Problem URL: https://leetcode.com/problems/two-sum/submissions/1718888558/
  * 
  * Auto-committed by LeetUp
- * Date: 2025-07-31T12:24:14.568Z
+ * Date: 2025-07-31T22:51:15.063Z
  */
 
 class Solution(object):
@@ -20,7 +20,7 @@ class Solution(object):
 
 /*
  * End of solution for: Two Sum
- * Committed at: 2025-07-31T12:24:14.568Z
+ * Committed at: 2025-07-31T22:51:15.063Z
  * 
  * This solution was automatically committed by LeetUp.
  * Visit: https://leetup.app
