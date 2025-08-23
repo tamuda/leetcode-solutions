0c402423e358a052039fc8762d6f656462eb84e5
Tamuda <118585051+tamuda@users.noreply.github.com>
2025-08-23 10:34:38 -0400

    Solve Two Sum [Easy] - python

--- problems/easy/two-sum.py
+++ problems/easy/two-sum.py
@@ -2,25 +2,31 @@
  * LeetCode Problem: Two Sum
  * Difficulty: Easy
  * Language: python
- * Problem URL: https://leetcode.com/problems/two-sum/submissions/1720823191/
+ * Problem URL: https://leetcode.com/problems/two-sum/submissions/1745486446/
  * 
  * Auto-committed by LeetUp
- * Date: 2025-08-02T15:22:52.964Z
+ * Date: 2025-08-23T14:34:37.245Z
  */
 
 class Solution(object):
     def twoSum(self, nums, target):
-        num_to_index = {}  # Dictionary to store number -> index
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_to_index:
-                return [num_to_index[complement], i]
-            num_to_index[num] = i
-
-
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        myset = {}
+        for idx, num in enumerate(nums):
+            comp = target - num
+            if comp in myset:
+                return [idx, myset[comp]]
+            else:
+                myset[num] = idx
+        return None
+        
 /*
  * End of solution for: Two Sum
- * Committed at: 2025-08-02T15:22:52.964Z
+ * Committed at: 2025-08-23T14:34:37.245Z
  * 
  * This solution was automatically committed by LeetUp.
  * Visit: https://leetup.app
